556ac13fdc13c68aeeeba3274725f6f659c0d87d
kimdahyee <diana6777@naver.com>
2021-09-20 14:30:42 +0900

    [BOJ] 2751 수 정렬하기 2

---
+++ Sort/src/Sorting_2751.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Sorting_2751 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		ArrayList<Integer> arr = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			arr.add(s.nextInt());
+		}
+		
+		Collections.sort(arr);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int element : arr) {
+			sb.append(element).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
