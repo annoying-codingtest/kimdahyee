0f418ab409b83e4a6d42db6f59d19fd620e22b8e
kimdahyee <diana6777@naver.com>
2021-09-21 21:30:47 +0900

    [BOJ] 10989 수 정렬하기 3

---
+++ Sort/src/Sorting_10989.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Sorting_10989 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedReader는 개행만 경계로 인식
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int element : arr) {
+			sb.append(element).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
