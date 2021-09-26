2792074e33db23e95e1d6a309b6bae89f85e63db
kimdahyee <diana6777@naver.com>
2021-09-27 00:02:52 +0900

    [BOJ] 11651 좌표 정렬하기 2

--- Sort/src/SortCoordinate2_11651.java
+++ Sort/src/SortCoordinate2_11651.java
@@ -12,13 +12,15 @@ public class SortCoordinate2_11651 {
 		int[][] arr = new int[n][2];
 		
 		for (int i = 0; i < n; i++) {
-			arr[i][0] = s.nextInt();
+			// StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = s.nextInt(); // Integer.parseInt(st.nextToken());
 			arr[i][1] = s.nextInt();
 		}
 		
 		Arrays.sort(arr, (e1, e2) -> {
-			if(e1[1] == e2[1]) {
-				return e1[0] - e2[0];
+			if(e1[1] == e2[1]) { // y값이 같으면
+				return e1[0] - e2[0]; // x값을 비교, 선행이 크면 양수가 받환 후행이 크면 음수가 반환
+				//자바는 오름차순이 디폴트라 음수가 나오면 선행이 후행 보다는 작다는 뜻이므로 위치 안바꾸고 양수면 바꿔
 			} else {
 				return e1[1] - e2[1];
 			}
