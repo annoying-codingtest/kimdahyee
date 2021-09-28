afbd76c7258563c510e154c897e4fb61b2973ed0
kimdahyee <diana6777@naver.com>
2021-09-29 02:19:41 +0900

    [BOJ] 10814 나이순 정렬

---
+++ Sort/src/SortByAge_10814.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class SortByAge_10814 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		int[][] age = new int[n][2];
+		String[][] name = new String[n][2];
+		for (int i = 0; i < n; i++) {
+			age[i][0] = i;
+			name[i][0] = String.valueOf(i); 
+			age[i][1] = s.nextInt();
+			name[i][1] = s.nextLine();			
+		}
+		
+		Arrays.sort(age, (e1, e2) -> {
+			if (e1[1] == e2[1]) { //나이가 같으면
+				return e1[0] - e2[0]; //등록순 
+			} else {
+				return e1[1] - e2[1];
+			}
+		});
+		
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			int index = age[i][0];
+			sb.append(age[i][1]).append(name[index][1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
