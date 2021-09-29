005490f34c4522dc67e5f4b6e8ee0f0492ae894e
kimdahyee <diana6777@naver.com>
2021-09-29 16:35:06 +0900

    [BOJ] 18870 좌표 압축

--- Sort/src/CompCoor_18870.java
+++ Sort/src/CompCoor_18870.java
@@ -10,21 +10,24 @@ public class CompCoor_18870 {
 		int n = Integer.parseInt(br.readLine());
 		String[] input =  br.readLine().split(" ");
 		
-		int[] numbers = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
-		int[] sortNumbers = numbers.clone();
+		int[] numbers = Arrays.stream(input).mapToInt(Integer::parseInt).toArray(); 
+		int[] sortNumbers = Arrays.stream(numbers.clone()).sorted().toArray();
 		
-		Arrays.sort(sortNumbers); 
+		/*
+		 * int[] sortNumbers = numbers.clone();
+		 * Arrays.sort(sortNumbers); 
+		 */
 		
 		Map<Integer, Integer> hm = new HashMap<>(); 
 		int index = 0;
-		for (int num : sortNumbers) {
+		for (int num : sortNumbers) { 
 			if (!hm.containsKey(num)) {
 				hm.put(num, index++);
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < n; i++) {
 			sb.append(hm.get(numbers[i])).append(" ");
 		}
 		System.out.println(sb);
