0870a89537370e5d61eb75d05cf5336ea98e76ac
kimdahyee <diana6777@naver.com>
2021-10-14 13:53:52 +0900

    [BOJ] 9012 괄호

---
+++ Stack/src/Parenthesis_9012.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Parenthesis_9012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuilder sb = new StringBuilder();
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		String[] input = new String[n];
+		for (int i = 0; i < n; i++) {
+			input[i] = s.next();		
+		}
+		
+		for (int i = 0; i < n; i++) {
+			String[] splitArray = new String[input[i].length()];
+			splitArray = input[i].split("");
+			
+			int pos = 0;
+			ArrayList<String> data = new ArrayList<>();
+			for (int j = 0; j < splitArray.length; j++) {
+				if (pos == 0) { // 스택이 비어있으면
+					data.add(pos++, splitArray[j]);
+				} else { 
+					if (data.get(pos - 1).equals("(") && splitArray[j].equals(")")) {
+						data.remove(--pos);
+					} else { 
+						data.add(pos++, splitArray[j]);
+					}
+				}
+			}
+			
+			if (data.size() == 0) {
+				sb.append("YES").append("\n");
+			} else {
+				sb.append("NO").append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}
