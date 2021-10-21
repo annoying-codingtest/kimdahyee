f4d6c771b5dfd0b4030fa8caf1b374cbd2e60a6e
kimdahyee <diana6777@naver.com>
2021-10-21 20:23:46 +0900

    [BOJ] 10773 제로 - Stack 클래스 사용

--- Stack/src/Zero_10773.java
+++ Stack/src/Zero_10773.java
@@ -4,6 +4,7 @@ public class Zero_10773 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Stack<Integer> stack = new Stack<>();
 		Scanner s = new Scanner(System.in);
 		int k = s.nextInt();
 		
@@ -15,15 +16,20 @@ public class Zero_10773 {
 			input[i] = s.nextInt();
 			
 			if (input[i] != 0) {
-				data[pos++] = input[i];
+				//data[pos++] = input[i];
+				stack.push(input[i]);
 			} else { 
-				data[--pos] = 0;
+				//data[--pos] = 0;
+				stack.pop();
 			}
 		}
 		
 		int sum = 0;
-		for (int i = 0; i < data.length; i++) {
-			sum += data[i];
+		/*
+		 * for (int i = 0; i < data.length; i++) { sum += data[i]; }
+		 */
+		for (int element : stack) {
+			sum += element;
 		}
 		System.out.println(sum);
 	}
