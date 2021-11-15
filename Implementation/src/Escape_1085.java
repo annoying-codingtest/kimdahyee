fe5457a229ca3d6c6926357ad0996b84de76134e
kimdahyee <diana6777@naver.com>
2021-11-16 08:57:48 +0900

    [BOJ] 1085 직사각형에서 탈출

---
+++ Implementation/src/Escape_1085.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Escape_1085 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		
+		int x = s.nextInt();
+		int y = s.nextInt();
+		int w = s.nextInt();
+		int h = s.nextInt();
+		
+		int min = Integer.MAX_VALUE;
+		
+		min = Math.min(min, x);
+		min = Math.min(min, y);
+		min = Math.min(min, w - x);
+		min = Math.min(min, h - y);
+		
+		System.out.println(min);
+	}
+
+}
