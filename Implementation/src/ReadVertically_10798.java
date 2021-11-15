0ef71c982495d65755096fa609c86caca126298f
kimdahyee <diana6777@naver.com>
2021-11-15 21:29:42 +0900

    [BOJ] 10798 세로읽기

---
+++ Implementation/src/ReadVertically_10798.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class ReadVertically_10798 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[][] input = new String[5][15];
+
+		for (int i = 0; i < 5; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < s.length(); j++) {
+				input[i][j] = String.valueOf(s.charAt(j));
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();		
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (input[j][i] == null) {
+					continue; 
+				} else {
+					sb.append(input[j][i]); 
+				} 
+			}
+		}
+		System.out.print(sb);
+	}
+}
