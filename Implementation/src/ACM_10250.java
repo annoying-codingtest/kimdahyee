81bdd1de5f09e21325d38d085de3f07861b9ee80
kimdahyee <diana6777@naver.com>
2021-11-17 10:37:16 +0900

    [BOJ] 10250 ACM 호텔

---
+++ Implementation/src/ACM_10250.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ACM_10250 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int t = s.nextInt();
+		for (int i = 0; i < t; i++) {
+			int h = s.nextInt();
+			int w = s.nextInt();
+			int n = s.nextInt();
+			
+			int x = n % h;
+			int y = n / h;
+			
+			if (x == 0) {
+				x = h;
+			} else {
+				y += 1; // 핵심
+			}
+			sb.append(x * 100 + y).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
