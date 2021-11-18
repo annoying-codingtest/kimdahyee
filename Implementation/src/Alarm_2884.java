6d06a075087c3916edbc318e274589c5f2c360fd
kimdahyee <diana6777@naver.com>
2021-11-18 10:21:27 +0900

    [BOJ] 2884 알람시계

---
+++ Implementation/src/Alarm_2884.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Alarm_2884 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		
+		int h = s.nextInt();
+		int m = s.nextInt();
+		
+		int hour = h, minute = m - 45;
+		
+		if (minute < 0) {
+			minute += 60;
+			if (h == 0) {
+				hour = 23;
+			} else {
+				hour = h - 1;
+			}
+		}
+		
+		System.out.println(hour + " " + minute);
+	}
+
+}
