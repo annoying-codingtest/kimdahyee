d8ad39af500c5c9277084236576f96a4835280c3
kimdahyee <diana6777@naver.com>
2021-11-28 19:43:40 +0900

    주석 추가

--- Implementation/src/Ant_3048.java
+++ Implementation/src/Ant_3048.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Ant_3048 {
 
-	static int N1;
-    static int N2;
-    static int T;
+	static int n1, n2, t;
     static ArrayList<Node> list = new ArrayList<>();
     static StringBuffer sb = new StringBuffer();
     
@@ -13,24 +11,25 @@ public class Ant_3048 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        N1 = Integer.parseInt(st.nextToken());
-        N2 = Integer.parseInt(st.nextToken());
+        n1 = Integer.parseInt(st.nextToken());
+        n2 = Integer.parseInt(st.nextToken());
         
         char[] temp1 = br.readLine().toCharArray();
         
-        for (int i = N1 - 1; i >= 0 ; i--) {
+        for (int i = n1 - 1; i >= 0 ; i--) { // 첫 번째 그룹(true)은 역순으로
             list.add(new Node(temp1[i], true));
         }
+        
         char[] temp2 = br.readLine().toCharArray();
         
-        for (int i = 0; i < N2; i++) {
-            list.add(new Node(temp2[i], false));
+        for (int i = 0; i < n2; i++) {
+            list.add(new Node(temp2[i], false)); // 두 번째 그룹(false)
         }
         
-        T = Integer.parseInt(br.readLine());
+        t = Integer.parseInt(br.readLine());
         
         // 시뮬레이션 이전에 체크
-        if (N2 + N1 - 1 <= T) {
+        if (n2 + n1 - 1 <= t) {
             for (int i = 0; i < temp2.length; i++) {
                 sb.append(temp2[i]);
             }
@@ -38,76 +37,30 @@ public class Ant_3048 {
             for (int i = temp1.length - 1; i >= 0; i--) {
                 sb.append(temp1[i]);
             }
+            
             System.out.println(sb.toString());
-            return ;
+            return;
         }
         
-        while (T --> 0) {
-            for (int i = 0; i < list.size()-1; i++) {
+        while (t --> 0) {
+            for (int i = 0; i < list.size() - 1; i++) {
                 Node cur = list.get(i);
-                Node next = list.get(i+1);
+                Node next = list.get(i + 1);
                 
                 if (!cur.team || cur.team == next.team) continue;
                 
                 list.set(i, next);
-                list.set(i+1, cur);
+                list.set(i + 1, cur);
                 i++;
             }
         }
+        
         for (int i = 0; i < list.size(); i++) {
             sb.append(list.get(i).c);
         }
         System.out.println(sb.toString());    
     }
-    
-	/*
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n1 = Integer.parseInt(st.nextToken());
-		int n2 = Integer.parseInt(st.nextToken());
-		
-		String s1 = br.readLine();
-		String s2 = br.readLine();
-		
-		int t = Integer.parseInt(br.readLine());
-		
-		StringBuffer stringBuffer = new StringBuffer(s1);
-		s1 = stringBuffer.reverse().toString();
-		
-		String concat = s1.concat(s2); // CBADEF
-		String[] current =  concat.split("");
-		
-		if (t == 0) {
-			System.out.println(concat);
-            return;
-		} else {
-			for (int i = 1; i <= t; i++) {
-				int start = n1 - i;
-				int end = n1 + (i - 1);
-				
-				if (start < 0 || end < 0) {
-					break;
-				}
-				
-				for (int j = start; j < end; j += 2) {
-					String tmp = current[j];
-					current[j] = current[j + 1];
-					current[j + 1] = tmp;
-				}
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < current.length; i++) {
-			sb.append(current[i]);
-		}
-		System.out.println(sb);
-	}
-	 */
+   
 }
 
 class Node {
@@ -119,3 +72,53 @@ class Node {
         this.team = team;
     }
 }
+
+/*
+public static void main(String[] args) throws IOException {
+	// TODO Auto-generated method stub
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	
+	int n1 = Integer.parseInt(st.nextToken());
+	int n2 = Integer.parseInt(st.nextToken());
+	
+	String s1 = br.readLine();
+	String s2 = br.readLine();
+	
+	int t = Integer.parseInt(br.readLine());
+	
+	StringBuffer stringBuffer = new StringBuffer(s1);
+	s1 = stringBuffer.reverse().toString();
+	
+	String concat = s1.concat(s2); // CBADEF
+	String[] current =  concat.split("");
+	
+	if (t == 0) {
+		System.out.println(concat);
+        return;
+	} else {
+		for (int i = 1; i <= t; i++) {
+			int start = n1 - i;
+			int end = n1 + (i - 1);
+			
+			if (start < 0 || end < 0) {
+				break;
+			}
+			
+			for (int j = start; j < end; j += 2) {
+				String tmp = current[j];
+				current[j] = current[j + 1];
+				current[j + 1] = tmp;
+			}
+		}
+	}
+	
+	StringBuilder sb = new StringBuilder();
+	for (int i = 0; i < current.length; i++) {
+		sb.append(current[i]);
+	}
+	System.out.println(sb);
+}
+ */
+
