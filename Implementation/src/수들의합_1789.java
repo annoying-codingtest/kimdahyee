5b58664211f3128848841021deaedc6c38ea9c97
kimdahyee <diana6777@naver.com>
2021-12-28 00:29:30 +0900

    [BOJ] 1789 수들의 합

--- Implementation/src/수들의합_1789.java
+++ Implementation/src/수들의합_1789.java
@@ -6,24 +6,19 @@ public class 수들의합_1789 {
 
 		Scanner s = new Scanner(System.in);
 		
-		int input = s.nextInt();
-		
-		boolean[] checked = new boolean[input + 1];
-		
-		int count = 0, sub = input;
+		// input type 주의하기
+		long input = s.nextLong();
+		long sum = 0;
+		int count = 0;
 		
 		for (int i = 1; i <= input; i++) {
-			if (!checked[i]) {
-				count++;
-				sub -= i;
-				checked[i] = true;
-			}
-
-			if (checked[sub] || sub == 0) {
-				System.out.println(count);
-				return;
-			}
+			if (sum > input) { break; }
+			sum += i;
+			count++;
 		}
+		
+		System.out.println(count - 1);
+		
 	}
 
 }
