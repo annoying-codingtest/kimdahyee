1fc35b73b1aed919245f25bf5458d87ec9955187
kimdahyee <diana6777@naver.com>
2022-01-23 15:20:11 +0900

    [BOJ] 5639 이진검색트리

---
+++ Tree/src/이진검색트리_5639.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class 이진검색트리_5639 {
+	static int[] tree = new int[10001];
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		
+		int idx = 0;
+		while(s.hasNext()) {
+			tree[idx++] = s.nextInt();
+		}
+		
+		postOrder(0, idx - 1);
+		// 시작이랑 끝을 넘겨
+	}
+
+	public static void postOrder(int n, int e) {
+		if (n > e) {
+			return;
+		}
+		
+		int mid = n + 1;
+		while (mid <= e && tree[mid] < tree[n]) {
+			mid++;
+		}
+		
+		postOrder(n + 1, mid - 1);
+		postOrder(mid, e);
+		System.out.println(tree[n]);
+	}
+}
+
